664a11d10d4f76ce1b5a8d1bcc91944fca9a6a29
Aditya Sridhar <aditya123ss@gmail.com>
2015-02-28 10:08:34 -0600

    changing menus again

--- base.py
+++ base.py
@@ -44,20 +44,18 @@ def get_order(order_id):
     return result
 
 
-#-------------------------------------------------------------------
+#--------Create Menu, Cook, Order----------------------------------------------
 
-# Stage 2 of I want to become a cook
-# submit menu details
-# MAY NEED TO SQUISH CREATE_MENU AND CREATE_ORDER
 @app.route('/create_menu', methods=['POST'])
 def create_menu():
     connection = httplib.HTTPSConnection('api.parse.com', 443)
     connection.connect()
     connection.request('POST', '/1/classes/Menu', json.dumps({
-        "Description": "Ramen", # request.form['Description'],
-        "Category": "Chinese", # request.form['Category'],
-        "dietaryRestriction": "Vegetarian", # request.form['dietaryRestriction']
+        "Description": "Pad Thai", # request.form['Description'],
+        "Category": "Thai", # request.form['Category'],
+        "dietaryRestriction": "None", # request.form['dietaryRestriction']
         "spicyLevel": 3,
+        "price": 4,
     }), {
         "X-Parse-Application-Id": "mL4QwznW8QOvKhqbG9DpDRn42Kpj4rETCeLLEMju",
         "X-Parse-REST-API-Key": "Ld88eQRGwvTfe7ocsG2Gn5K942B9s8dOTlhGEvEV",
@@ -66,20 +64,21 @@ def create_menu():
     result = json.loads(connection.getresponse().read())
     # If successful, result contains objectId, and createdAt
     # In case of an error, result contains the error message
-    return "result" # This should be a success message or error message
+    return result['objectId'] # This should be a success message or error message
 
 
-# Stage 3 of I want to become a cook
-# Menu_id, Cook_id and hungry_id = undefined
 @app.route('/create_order', methods=['POST'])
 def create_order():
     connection = httplib.HTTPSConnection('api.parse.com', 443)
     connection.connect()
-    cook_id = "XGoOxKcOwL"
-    hungry_id = "someUserId" # hungry could either be undefined or a user_id
+    cook_id = "8sCez1IjcN"
+    hungry_id = "someUserId"
+
+    # Ultimately, we need the cookId and the hungryId
+    # But we may have to take cookId off of menuId
     connection.request('POST', '/1/classes/Order', json.dumps({
-        "cookId": cook_id, # request.form['Description'],
-        "hungryId": hungry_id, # request.form['Category'],
+        "cookId": cook_id,
+        "hungryId": hungry_id,
     }), {
         "X-Parse-Application-Id": "mL4QwznW8QOvKhqbG9DpDRn42Kpj4rETCeLLEMju",
         "X-Parse-REST-API-Key": "Ld88eQRGwvTfe7ocsG2Gn5K942B9s8dOTlhGEvEV",
@@ -89,14 +88,12 @@ def create_order():
     return "result"
 
 
-# Stage 1 of I want to become a cook
-# Submit user object and capacity_remaining
 @app.route('/create_cook', methods=['POST'])
 def create_cook():
     connection = httplib.HTTPSConnection('api.parse.com', 443)
     connection.connect()
     user_id = "ulR5Pcv0Qw" # Wont be using POST. Will have to take from user logged in
-    menu_id = "enterSomeMenuId"
+    menu_id = "U6v3vOo8no"
     capacity_remaining = 3
     # TODOCreate only if prev user_id not used
     connection.request('POST', '/1/classes/Cook', json.dumps({
@@ -118,6 +115,19 @@ def create_cook():
     })
     result = json.loads(connection.getresponse().read()) 
     return "result"
+
+
+# --Get Menu/Menus--------------------------------------------------------------------
+
+
+@app.route('/get_all_menus', methods=['GET'])
+def get_all_menus():
+    connection = httplib.HTTPSConnection('api.parse.com', 443)
+    connection.connect()
+    connection.request('GET', '/1/classes/Menu', )    
+
+
+# --Update------------------------------------------------------------------
 
 
 @app.route('/update_capacity_remaining/<cook_id>', methods=['PUT'])
@@ -145,21 +155,6 @@ def update_capacity_remaining(cook_id):
     print result
     return result
 
-
-@app.route('/update_order_with_hungry_user/<order_id>/<hungry_id>', methods=['POST'])
-def update_order_with_hungry_user(order_id, hungry_id):
-    # TODO: A cook cannot be hungry
-    connection = httplib.HTTPSConnection('api.parse.com', 443)
-    connection.connect()
-
-    connection.request('PUT', '/1/classes/Order/'+order_id, json.dumps({
-       "hungryId": hungry_id
-    }), {
-       "X-Parse-Application-Id": "mL4QwznW8QOvKhqbG9DpDRn42Kpj4rETCeLLEMju",
-       "X-Parse-REST-API-Key": "Ld88eQRGwvTfe7ocsG2Gn5K942B9s8dOTlhGEvEV",
-       "Content-Type": "application/json"
-    })
-    return "result"
 
 #----------Authentication------------------------------------------------
 
